41de98b08ca66759f77f4460f09e0013b3c6a409
ivaanjm <jurado.marquez.ivan@alumnat.copernic.cat>
2021-04-05 17:19:53 +0200

    Classe SingleSession

---
+++ src/main/java/m06/uf2/logic/main/SingleSession.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m06.uf2.logic.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+
+    public class SingleSession {
+
+        private static SingleSession instancia = null;
+        private static SessionFactory sesio = null;
+        
+        private SingleSession() {
+            InitSessionFactory();
+        }
+        
+        private static SingleSession getInstance()
+        {
+            if (instancia == null)
+                instancia = new SingleSession();
+            
+            return instancia;
+        }
+        
+        private void InitSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties());
+		this.sesio = configuration.buildSessionFactory(builder.build());
+	}
+
+        public static Session getSesio() {
+                getInstance();
+            return sesio.openSession();
+        }
+}
+
