b02d5cebefff0b13fc69c6355d270e688b44f668
ivaanjm <jurado.marquez.ivan@alumnat.copernic.cat>
2021-04-05 17:05:46 +0200

    Primer Commit

---
+++ src/main/java/com/mycompany/practica1uf2/Prueba.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica1uf2;
+
+/**
+ *
+ * @author mitra
+ */
+public class Prueba {
+    
+}
